20ebe58e23aae7e4680fc27c8d1a0f55025e1541
Justin Lazarus <justin.lazarus@gmail.com>
2022-02-07 20:27:17 -0800

    adding some documentation

--- src/game.rs
+++ src/game.rs
@@ -11,15 +11,27 @@ pub enum TileColor {
     Black,
 }
 
+/// Each Rummikub ® tile has only 2 identifying characteristics, a number and
+/// color. A standard game has 104 of these tiles plus an additional pair of
+/// jokers. Though the jokers in the standard game are usually colored black and
+/// red, these colors are not meaningful as jokers can be used to substitute any
+/// tile in the game.
 pub struct Tile {
     color: TileColor,
     number: u8,
 }
 
+/// Each player has a rack to hold tiles. When a player draws tiles from the
+/// pool at the beginning of the game and on each following turn, the tiles are
+/// placed on the rack. Each tile on the rack can be played individually or as
+/// part of a tile set, either a run or a group.
 pub struct Rack {
     tiles: Vec<Tile>,
 }
 
+/// The pool is usually a bag or some container to house tiles that aren't in
+/// play. Players take tiles out of the pool at the beginning of the game and
+/// during each round of play.
 pub struct Pool {
     tiles: Vec<Tile>,
 }
@@ -49,11 +61,17 @@ impl Pool {
     }
 }
 
+/// When players build sets of tiles (runs and groups), they are played on the
+/// table. Once tiles are on the table as runs or groups, they are open for
+/// modification by all players.
 pub struct Table {
     runs: Vec<Run>,
     groups: Vec<Group>,
 }
 
+/// A run is composed of three or more same-colored tiles in consecutive
+/// number order. (A 1 may not follow a 13.) For example: red7, red8, red9.
+/// A run has a length of minimum 3 and maximum 13 tiles.
 pub struct Run {
     color: TileColor,
     tiles: Vec<Tile>,
@@ -63,10 +81,11 @@ impl Run {
     fn len(&self) -> usize {
         self.tiles.len()
     }
-
-
 }
 
+/// A group is made from three or four same-value tiles in distinct colors. For
+/// example: red3, blue3, black3, yellow3. A group has a length of minimum3 and
+/// maximum 4 tiles.
 pub struct Group {
     number: u8,
     tiles: Vec<Tile>,
@@ -88,5 +107,4 @@ mod tests {
 
         assert_eq!(pool.tiles.len(), 103);
     }
-
 }
